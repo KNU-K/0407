a9364bb1be0152c7d86fd80441010065c1e7762f
khwww <khw4420@knu.ac.kr>
2024-04-10 17:28:25 +0900

    미리보기 이미지 수정2

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
 export const EXAMPLE_PATH = "blog-starter";
 export const CMS_NAME = "Markdown";
-export const HOME_OG_IMAGE_URL =
-  "0407\public\images\cover.png";
+export const HOME_OG_IMAGE_URL = "/images/cover.png";
+
